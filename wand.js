471e564b127c5892f068dec5a58b43cfc897973d
Nick Battjes <nhbattjes@gmail.com>
2018-08-28 10:15:44 -0700

    got basic wand in as cursor

---
+++ wand.js
@@ -0,0 +1,16 @@
+class Wand {
+    constructor(ctx, canvas) {
+        this.canvasHeight = canvas.height;
+        this.canvasWidth = canvas.width;
+        this.ctx = ctx;
+        this.wandWidth = 20;
+        this.wandLength = 80;
+        this.wandTipLength = 20;
+        this.wandX = 50;
+        this.wandY = 50;
+    }
+
+    drawWand() {
+
+    }
+}
