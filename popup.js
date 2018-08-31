1cab721a8e09582e3a63149cd2dd5e78c6ff47e5
Nate Harris <nathanharris.sf@gmail.com>
2018-08-31 15:12:37 -0700

    File clean up and various updates

--- popup.js
+++ popup.js
@@ -2,28 +2,22 @@ let button1 = document.getElementById('ZapEverything');
 
 button1.addEventListener("click", function() {
   chrome.tabs.query({ active:true, currentWindow: true}, function(tabs) {
-    console.log("click regiestered in popup");
-
     chrome.tabs.sendMessage(tabs[0].id, {type: `${button1.id}`});
-  })
+  });
 });
 
 let button2 = document.getElementById('ZapElement');
 
 button2.addEventListener("click", function() {
   chrome.tabs.query({ active:true, currentWindow: true}, function(tabs) {
-    console.log("click regiestered in popup");
-
     chrome.tabs.sendMessage(tabs[0].id, {type: `${button2.id}`});
-  })
+  });
 });
 
 let button3 = document.getElementById('SmashElement');
 
 button3.addEventListener("click", function () {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    console.log("click regiestered in popup");
-
     chrome.tabs.sendMessage(tabs[0].id, { type: `${button3.id}` });
-  })
+  });
 });
